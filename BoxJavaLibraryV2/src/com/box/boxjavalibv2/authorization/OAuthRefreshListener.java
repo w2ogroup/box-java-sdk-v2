754b92c4ed6bc0dfd348fd3001750ba7fcc6753d
jian lin <jian@box.com>
2013-07-15 14:01:44 -0700

    add description for OAuthRefreshListener

--- BoxJavaLibraryV2/src/com/box/boxjavalibv2/authorization/OAuthRefreshListener.java
+++ BoxJavaLibraryV2/src/com/box/boxjavalibv2/authorization/OAuthRefreshListener.java
@@ -2,7 +2,14 @@ package com.box.boxjavalibv2.authorization;
 
 import com.box.boxjavalibv2.interfaces.IAuthData;
 
+/**
+ * Listener listening to the event that oauth token is refreshed.
+ */
 public interface OAuthRefreshListener {
 
+    /**
+     * @param newAuthData
+     *            the new IAuthData(after refresh).
+     */
     void onRefresh(IAuthData newAuthData);
 }
